71aa6f1c8409a7520421acead714965cf6487399
Sam King <kingst@gmail.com>
2019-04-05 16:38:57 -0700

    Improve bin checking, shorter error correction duration (#6)

--- CardScan/Classes/CreditCardUtils.swift
+++ CardScan/Classes/CreditCardUtils.swift
@@ -7,9 +7,7 @@ struct CreditCardUtils {
     static func luhnCheck(_ cardNumber: String) -> Bool {
         if cardNumber.count == 0 {
             return false
-        } else if cardNumber.count == 16 && !cardNumber.starts(with: "4") && !cardNumber.starts(with: "5") {
-            return false
-        } else if cardNumber.count == 15 && !cardNumber.starts(with: "3") {
+        } else if !isValidBin(number: cardNumber) {
             return false
         }
         
@@ -25,6 +23,46 @@ struct CreditCardUtils {
         }
         return sum % 10 == 0
     }
+    
+    static func isValidBin(number: String) -> Bool {
+        return isAmex(number: number) || isDiscover(number: number) || isVisa(number: number) || isMastercard(number: number)
+    }
+    
+    static func isAmex(number: String) -> Bool {
+        guard let prefix = Int(String(number.prefix(2))) else {
+            return false
+        }
+        
+        return number.count == 15 && (prefix == 34 || prefix == 37)
+    }
+    
+    static func isDiscover(number: String) -> Bool {
+        guard let prefix2 = Int(String(number.prefix(2))), let prefix4 = Int(String(number.prefix(4))), let prefix6 = Int(String(number.prefix(6))) else {
+            return false
+        }
+        
+        return prefix2 == 64 || prefix2 == 65 || prefix4 == 6011 ||
+            (prefix6 >= 622126 && prefix6 <= 622925) ||
+            (prefix6 >= 624000 && prefix6 <= 626999) ||
+            (prefix6 >= 628200 && prefix6 <= 628899)
+    }
+    
+    static func isMastercard(number: String) -> Bool {
+        guard let prefix2 = Int(String(number.prefix(2))), let prefix4 = Int(String(number.prefix(4))) else {
+            return false
+        }
+        
+        if number.count != 16 {
+            return false
+        }
+        
+        return (prefix2 >= 51 && prefix2 <= 55) || (prefix4 >= 2221 && prefix4 <= 2720)
+    }
+    
+    static func isVisa(number: String) -> Bool {
+        return number.count == 16 && number.starts(with: "4")
+    }
+    
     
     static func format(number: String) -> String {
         if number.count == 16 {
