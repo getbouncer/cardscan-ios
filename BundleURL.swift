887603b44e3e51a624d894505cf3be9e5be3474c
Jaime Park <jaimepark@Jaimes-MacBook-Pro.local>
2019-06-27 10:05:10 -0700

    edits to bundleURL

--- BundleURL.swift
+++ BundleURL.swift
@@ -3,30 +3,40 @@ import CoreML
 @available(macOS 10.13, iOS 11.0, tvOS 11.0, watchOS 4.0, *)
 //model name : "FindFour.mlmodelc"
 struct BundleURL{
-    static func bundleUrl(modelName: String) -> URL?{
-        var modelcFile: URL
-        if let documentDirectory =
-            try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor:nil, create:false) {
-                modelcFile = documentDirectory.appendingPathComponent(modelName)
-            }
+    static func bundleUrl(forResource: String, withExtension: String, modelName: String) -> URL?{
+        guard let documentDirectory =
+            try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor:nil, create:false) else {
+                print("Directory could not be found")
+                return nil
+        }
+        
+        let modelcFile = documentDirectory.appendingPathComponent(modelName)
         
         if !FileManager.default.fileExists(atPath: modelcFile.path) {
             guard let bundleUrl = Bundle(for: FindFour.self).url(forResource: "CardScan", withExtension: "bundle") else {
-                print("Could not find bundleURL")
+                print("bundleURL could not be found")
                 return nil
             }
             
             guard let bundle = Bundle(url: bundleUrl) else {
+                print("bundle with bundleURL could not be found")
                 return nil
             }
             
-            let modelUrl = bundle.url(forResource: "FindFour", withExtension: "bin")
-            
-            if let compiledUrl =
-                try? MLModel.compileModel(at: modelUrl){
-                try? FileManager.default.moveItem(at: compiledUrl!, to: modelcFile)
+            guard let modelUrl = bundle.url(forResource: forResource, withExtension: withExtension) else {
+                print("Could not find bundle named \" FindFour.bin\" ")
+                return nil
             }
-        
+            
+            guard let compiledUrl = try? MLModel.compileModel(at: modelUrl) else {
+                print("Model could not compile")
+                return nil
+            }
+            
+            guard let _ = try? FileManager.default.moveItem(at: compiledUrl, to: modelcFile) else {
+                print("Could not move to modelcFile")
+                return nil
+            }
         }
         
         return modelcFile
