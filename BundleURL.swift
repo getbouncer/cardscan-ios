b9461f9b5e739db22f6ea4a45d686125459d4bdb
Jaime Park <jaimepark@Jaimes-MacBook-Pro.local>
2019-06-26 23:52:01 -0700

    added guard statements

--- BundleURL.swift
+++ BundleURL.swift
@@ -1,20 +1,34 @@
 import CoreML
 
 @available(macOS 10.13, iOS 11.0, tvOS 11.0, watchOS 4.0, *)
-class var BundleURL: URL{
-    /// URL of model assuming it was installed in the same bundle as this class
-    let documentDirectory = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor:nil, create:false)
-    let modelcFile = documentDirectory.appendingPathComponent("FindFour.mlmodelc")
-    
-    if !FileManager.default.fileExists(atPath: modelcFile.path) {
-        if let bundleUrl = Bundle(for: FindFour.self).url(forResource: "CardScan", withExtension: "bundle"){
-            if let bundle = Bundle(url: bundleUrl){
-                if let modelUrl = bundle.url(forResource: "FindFour", withExtension: "bin"){
-                    let compiledUrl = try? MLModel.compileModel(at: modelUrl)
-                    try? FileManager.default.moveItem(at: compiledUrl!, to: modelcFile)
-                }
+//model name : "FindFour.mlmodelc"
+struct BundleURL{
+    static func bundleUrl(modelName: String) -> URL?{
+        var modelcFile: URL
+        if let documentDirectory =
+            try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor:nil, create:false) {
+                modelcFile = documentDirectory.appendingPathComponent(modelName)
             }
+        
+        if !FileManager.default.fileExists(atPath: modelcFile.path) {
+            guard let bundleUrl = Bundle(for: FindFour.self).url(forResource: "CardScan", withExtension: "bundle") else {
+                print("Could not find bundleURL")
+                return nil
+            }
+            
+            guard let bundle = Bundle(url: bundleUrl) else {
+                return nil
+            }
+            
+            let modelUrl = bundle.url(forResource: "FindFour", withExtension: "bin")
+            
+            if let compiledUrl =
+                try? MLModel.compileModel(at: modelUrl){
+                try? FileManager.default.moveItem(at: compiledUrl!, to: modelcFile)
+            }
+        
         }
+        
+        return modelcFile
     }
-    return modelcFile
 }
