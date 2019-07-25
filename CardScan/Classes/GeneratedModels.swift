95abce067b113ee3d80db64116d78eb4a01f8efd
Sam King <kingst@gmail.com>
2019-07-24 22:01:05 -0700

    Use pre-compiled models instead of compiling them on the device

--- CardScan/Classes/GeneratedModels.swift
+++ CardScan/Classes/GeneratedModels.swift
@@ -1,13 +1,11 @@
-
 //
-// This file was automatically generated and edited.
+// FindFour.swift
+//
+// This file was automatically generated and should not be edited.
 //
 
 import CoreML
 
-enum InBackgroundError: Error {
-    case appIsInTheBackground
-}
 
 /// Model Prediction Input Type
 @available(macOS 10.13, iOS 11.0, tvOS 11.0, watchOS 4.0, *)
@@ -70,7 +68,12 @@ class FindFourOutput : MLFeatureProvider {
 @available(macOS 10.13, iOS 11.0, tvOS 11.0, watchOS 4.0, *)
 class FindFour {
     var model: MLModel
-
+    
+    /// URL of model assuming it was installed in the same bundle as this class
+    class var urlOfModelInThisBundle : URL {
+        let bundle = Bundle(for: FindFour.self)
+        return bundle.url(forResource: "FindFour", withExtension:"mlmodelc")!
+    }
     
     /**
      Construct a model with explicit path to mlmodelc file
@@ -81,7 +84,23 @@ class FindFour {
     init(contentsOf url: URL) throws {
         self.model = try MLModel(contentsOf: url)
     }
-
+    
+    /// Construct a model that automatically loads the model from the app's bundle
+    convenience init() {
+        try! self.init(contentsOf: type(of:self).urlOfModelInThisBundle)
+    }
+    
+    /**
+     Construct a model with configuration
+     - parameters:
+     - configuration: the desired model configuration
+     - throws: an NSError object that describes the problem
+     */
+    @available(macOS 10.14, iOS 12.0, tvOS 12.0, watchOS 5.0, *)
+    convenience init(configuration: MLModelConfiguration) throws {
+        try self.init(contentsOf: type(of:self).urlOfModelInThisBundle, configuration: configuration)
+    }
+    
     /**
      Construct a model with explicit path to mlmodelc file and configuration
      - parameters:
@@ -114,9 +133,6 @@ class FindFour {
      - returns: the result of the prediction as FindFourOutput
      */
     func prediction(input: FindFourInput, options: MLPredictionOptions) throws -> FindFourOutput {
-        if AppState.inBackground {
-            throw InBackgroundError.appIsInTheBackground
-        }
         let outFeatures = try model.prediction(from: input, options:options)
         return FindFourOutput(features: outFeatures)
     }
@@ -131,6 +147,28 @@ class FindFour {
     func prediction(input1: CVPixelBuffer) throws -> FindFourOutput {
         let input_ = FindFourInput(input1: input1)
         return try self.prediction(input: input_)
+    }
+    
+    /**
+     Make a batch prediction using the structured interface
+     - parameters:
+     - inputs: the inputs to the prediction as [FindFourInput]
+     - options: prediction options
+     - throws: an NSError object that describes the problem
+     - returns: the result of the prediction as [FindFourOutput]
+     */
+    @available(macOS 10.14, iOS 12.0, tvOS 12.0, watchOS 5.0, *)
+    func predictions(inputs: [FindFourInput], options: MLPredictionOptions = MLPredictionOptions()) throws -> [FindFourOutput] {
+        let batchIn = MLArrayBatchProvider(array: inputs)
+        let batchOut = try model.predictions(from: batchIn, options: options)
+        var results : [FindFourOutput] = []
+        results.reserveCapacity(inputs.count)
+        for i in 0..<batchOut.count {
+            let outProvider = batchOut.features(at: i)
+            let result =  FindFourOutput(features: outProvider)
+            results.append(result)
+        }
+        return results
     }
 }
 
@@ -195,7 +233,13 @@ class FourRecognizeOutput : MLFeatureProvider {
 @available(macOS 10.13, iOS 11.0, tvOS 11.0, watchOS 4.0, *)
 class FourRecognize {
     var model: MLModel
-
+    
+    /// URL of model assuming it was installed in the same bundle as this class
+    class var urlOfModelInThisBundle : URL {
+        let bundle = Bundle(for: FourRecognize.self)
+        return bundle.url(forResource: "FourRecognize", withExtension:"mlmodelc")!
+    }
+    
     /**
      Construct a model with explicit path to mlmodelc file
      - parameters:
@@ -204,6 +248,22 @@ class FourRecognize {
      */
     init(contentsOf url: URL) throws {
         self.model = try MLModel(contentsOf: url)
+    }
+    
+    /// Construct a model that automatically loads the model from the app's bundle
+    convenience init() {
+        try! self.init(contentsOf: type(of:self).urlOfModelInThisBundle)
+    }
+    
+    /**
+     Construct a model with configuration
+     - parameters:
+     - configuration: the desired model configuration
+     - throws: an NSError object that describes the problem
+     */
+    @available(macOS 10.14, iOS 12.0, tvOS 12.0, watchOS 5.0, *)
+    convenience init(configuration: MLModelConfiguration) throws {
+        try self.init(contentsOf: type(of:self).urlOfModelInThisBundle, configuration: configuration)
     }
     
     /**
@@ -238,9 +298,6 @@ class FourRecognize {
      - returns: the result of the prediction as FourRecognizeOutput
      */
     func prediction(input: FourRecognizeInput, options: MLPredictionOptions) throws -> FourRecognizeOutput {
-        if AppState.inBackground {
-            throw InBackgroundError.appIsInTheBackground
-        }
         let outFeatures = try model.prediction(from: input, options:options)
         return FourRecognizeOutput(features: outFeatures)
     }
@@ -255,5 +312,27 @@ class FourRecognize {
     func prediction(input1: CVPixelBuffer) throws -> FourRecognizeOutput {
         let input_ = FourRecognizeInput(input1: input1)
         return try self.prediction(input: input_)
+    }
+    
+    /**
+     Make a batch prediction using the structured interface
+     - parameters:
+     - inputs: the inputs to the prediction as [FourRecognizeInput]
+     - options: prediction options
+     - throws: an NSError object that describes the problem
+     - returns: the result of the prediction as [FourRecognizeOutput]
+     */
+    @available(macOS 10.14, iOS 12.0, tvOS 12.0, watchOS 5.0, *)
+    func predictions(inputs: [FourRecognizeInput], options: MLPredictionOptions = MLPredictionOptions()) throws -> [FourRecognizeOutput] {
+        let batchIn = MLArrayBatchProvider(array: inputs)
+        let batchOut = try model.predictions(from: batchIn, options: options)
+        var results : [FourRecognizeOutput] = []
+        results.reserveCapacity(inputs.count)
+        for i in 0..<batchOut.count {
+            let outProvider = batchOut.features(at: i)
+            let result =  FourRecognizeOutput(features: outProvider)
+            results.append(result)
+        }
+        return results
     }
 }
