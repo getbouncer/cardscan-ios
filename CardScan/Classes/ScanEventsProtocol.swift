3d64bf97c84f72e8e34ff64373da7d6cfd41dd60
Sam King <kingst@gmail.com>
2019-08-09 17:02:46 -0700

    Add comment

--- CardScan/Classes/ScanEventsProtocol.swift
+++ CardScan/Classes/ScanEventsProtocol.swift
@@ -1,3 +1,11 @@
+//
+// This protocol provides extensibility for inspecting scanning results as they
+// happen. As the model detects a cc number it will invoke `onNumberRecognized`
+// and when it's done it notifies via `onScanComplete`.
+//
+// Both of these methods will always be invoked on the machineLearningQueue
+// serial dispatch queue.
+//
 public protocol ScanEvents {
     mutating func onNumberRecognized(number: String, expiry: Expiry?, cardImage: CGImage, numberBoundingBox: CGRect, expiryBoundingBox: CGRect?)
     mutating func onScanComplete(scanStats: ScanStats)
