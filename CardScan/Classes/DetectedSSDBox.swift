a58e479b485b3bc5caf40e1781e6b6f915497b20
Zain <dinxaen@gmail.com>
2019-08-21 08:59:55 -0700

    normalize in the toDict() method

--- CardScan/Classes/DetectedSSDBox.swift
+++ CardScan/Classes/DetectedSSDBox.swift
@@ -11,19 +11,24 @@ struct DetectedSSDBox {
     let rect: CGRect
     let label: Int
     let confidence: Float
+    let imgSize: CGSize
     
     init(category: Int, conf: Float, XMin: Double, YMin: Double, XMax: Double, YMax: Double, imageSize: CGSize){
-        // XMin, YMin, XMax, YMax represent normalized co-ordinates.
-        // To return co-ordinates with reference to the current image, take the product of
-        // XMax or XMin and Double(imageSize.width), YMax or YMin and Double(imageSize.height)
+
+        let XMin_ = XMin * Double(imageSize.width)
+        let XMax_ = XMax * Double(imageSize.width)
+        let YMin_ = YMin * Double(imageSize.height)
+        let YMax_ = YMax * Double(imageSize.height)
 
         self.label = category
         self.confidence = conf
-        self.rect = CGRect(x: XMin, y: YMin, width: XMax - XMin, height: YMax - YMin)
+        self.rect = CGRect(x: XMin_, y: YMin_, width: XMax_ - XMin_, height: YMax_ - YMin_)
+        self.imgSize = imageSize
     }
     
      func toDict() -> [String:Any]{
-        let objectDict = ["XMin": self.rect.minX, "YMin": self.rect.minY, "Height": self.rect.height, "Width":self.rect.width, "Label": self.label, "Confidence": self.confidence] as [String : Any]
+        let objectDict = ["XMin": self.rect.minX/self.imgSize.width, "YMin": self.rect.minY/self.imgSize.height, "Height": self.rect.height/self.imgSize.height,
+                          "Width":self.rect.width/self.imgSize.width, "Label": self.label, "Confidence": self.confidence] as [String : Any]
         
         
         return objectDict
