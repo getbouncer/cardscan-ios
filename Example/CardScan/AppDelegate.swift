80c505025d45fa55ffe981039f328c7650010959
Sam King <kingst@gmail.com>
2019-09-16 20:02:49 -0700

    Accidentally committed test code

--- Example/CardScan/AppDelegate.swift
+++ Example/CardScan/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        ScanViewController.configure(apiKey: "asdf")
+        //ScanViewController.configure()
         return true
     }
 
