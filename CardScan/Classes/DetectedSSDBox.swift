e08a3e0598122eef7c764101ae0abf887f76b88e
Zain <dinxaen@gmail.com>
2020-02-06 09:55:22 -0800

    make changes in the information we send to the server to keep android and ios consistent

--- CardScan/Classes/DetectedSSDBox.swift
+++ CardScan/Classes/DetectedSSDBox.swift
@@ -20,22 +20,23 @@ struct DetectedSSDBox {
         let YMin_ = YMin * Double(imageSize.height)
         let YMax_ = YMax * Double(imageSize.height)
         
-        // The model ouputs labels that are off by 1
-        // compared to the previous versions and this line
-        // serves to retain the consistency. This label
-        // correction should be removed in the future.
-        self.label = category - 1
+        
+        self.label = category
         self.confidence = conf
         self.rect = CGRect(x: XMin_, y: YMin_, width: XMax_ - XMin_, height: YMax_ - YMin_)
         self.imgSize = imageSize
     }
     
      func toDict() -> [String: Any] {
+        // The model ouputs labels that are off by 1
+        // compared to the previous versions and this line
+        // serves to retain the consistency. This label
+        // correction should be removed in the future.
         return ["x_min": self.rect.minX/self.imgSize.width,
                 "y_min": self.rect.minY/self.imgSize.height,
                 "height": self.rect.height/self.imgSize.height,
                 "width":self.rect.width/self.imgSize.width,
-                "label": self.label,
+                "label": self.label - 1,
                 "confidence": self.confidence]
     }
 }
