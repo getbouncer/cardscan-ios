9be7ed116516581ba7c77fc34a94ef1f89e0fa89
jaimejiyepark <jaimejiyepark@gmail.com>
2020-03-10 11:06:49 -0700

    landscape changes

---
+++ Example/CardScan/ModalViewViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ModalViewViewController.swift
+//  CardScan_Example
+//
+//  Created by Jaime Park on 3/10/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CardScan
+
+class ModalViewViewController: UIViewController, ScanDelegate {
+    func userDidCancel(_ scanViewController: ScanViewController) {
+        self.navigationController?.popToRootViewController(animated: true)
+        print("User Did Cancel")
+    }
+    
+    func userDidScanCard(_ scanViewController: ScanViewController, creditCard: CreditCard) {
+        self.navigationController?.popToRootViewController(animated: true)
+        print("User Did Scan Card \(creditCard.number)")
+    }
+    
+    func userDidSkip(_ scanViewController: ScanViewController) {
+        self.navigationController?.popToRootViewController(animated: true)
+        print("User Did Skip")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func toScanViewPress(_ sender: Any) {
+        guard let vc = ScanViewController.createViewController(withDelegate: self) else {
+            return
+        }
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
