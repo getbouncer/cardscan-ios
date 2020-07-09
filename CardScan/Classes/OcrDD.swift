d726af3a54c7e3d5fc1a992fd3579f0f6ea20165
Sam King <kingst@gmail.com>
2020-07-09 06:47:49 -0700

    Cache the OCR model (#182)

--- CardScan/Classes/OcrDD.swift
+++ CardScan/Classes/OcrDD.swift
@@ -7,20 +7,18 @@
 
 import Foundation
 
+@available(iOS 11.2, *)
 public class OcrDD{
     public var lastDetectedBoxes: [CGRect] = []
-    public init(){}
+    var ssdOcr = SSDOcrDetect()
+    public init() { }
 
     static func configure(){
-        if #available(iOS 11.2, *){
-            let ssdOcr = SSDOcrDetect()
-            ssdOcr.warmUp()
-        }
+        let ssdOcr = SSDOcrDetect()
+        ssdOcr.warmUp()
     }
 
-    @available(iOS 11.2, *)
     public func perform(croppedCardImage: CGImage) -> String?{
-        var ssdOcr = SSDOcrDetect()
         let number = ssdOcr.predict(image: UIImage(cgImage: croppedCardImage))
         self.lastDetectedBoxes = ssdOcr.lastDetectedBoxes
         return number
