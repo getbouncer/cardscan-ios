e41fdebbe100f57793054981f4391f279df68947
jaimejiyepark <jaimejiyepark@gmail.com>
2020-07-29 18:18:13 -0700

    added model config api call

---
+++ Example/CardScan_ExampleTests/CardScan_ModelDownloadConfigTests.swift
@@ -0,0 +1,24 @@
+//
+//  CardScan_ModelDownloadConfigTests.swift
+//  CardScan_ExampleTests
+//
+//  Created by Jaime Park on 7/29/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import XCTest
+@testable import CardScan
+
+class CardScan_ModelDownloadConfigTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testModelConfigDownload() {
+    // TODO(Jaime): get unit test to work with signing?
+    }
+}
