da9bfa8b5e4b8cfe61e07eac6bc4e7adfd2d02bc
jaimejiyepark <jaimejiyepark@gmail.com>
2020-07-31 11:54:29 -0700

    added unit test

--- Example/CardScan_ExampleTests/CardScan_ModelDownloadConfigTests.swift
+++ Example/CardScan_ExampleTests/CardScan_ModelDownloadConfigTests.swift
@@ -19,10 +19,27 @@ class CardScan_ModelDownloadConfigTests: XCTestCase {
     }
     
     func testModelConfigDownload() {
-    // TODO(Jaime): get unit test to work with signing?
+        let expectation = XCTestExpectation()
+        Api.apiKey = "<Put in proper api key>"
+        Api.getModelDownloadConfig(endpoint: "/v1/model/ios/bob/1", parameters: [:]) { response, error in
+            guard let _ = response, error == nil else {
+                return
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 20.0)
     }
     
     func testUrlWithQueryParams() {
-    // TODO(Jaime): test api func urlWithQueryParam escaping `+`
+        guard let urlNoParam = Api.urlWithQueryParameters(baseUrl: "https://api.getbouncer.com", endpoint: "/test", parameters: [:]),
+            let urlParam = Api.urlWithQueryParameters(baseUrl: "https://api.getbouncer.com", endpoint: "/test", parameters: ["1":"a"]),
+            let urlPlusParam = Api.urlWithQueryParameters(baseUrl: "https://api.getbouncer.com", endpoint: "/test", parameters: ["1":"a+b"])else {
+            XCTAssert(false)
+            return
+        }
+        
+        XCTAssert(urlNoParam.absoluteString == "https://api.getbouncer.com/test?")
+        XCTAssert(urlParam.absoluteString == "https://api.getbouncer.com/test?1=a")
+        XCTAssert(urlPlusParam.absoluteString == "https://api.getbouncer.com/test?1=a%2Bb")
     }
 }
