91c5eb72d3157debe73d32d51f569dabfc87d9d7
jaimejiyepark <jaimejiyepark@gmail.com>
2020-12-02 18:38:05 -0800

    modify strip binary

--- Package.swift
+++ Package.swift
@@ -16,14 +16,21 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "Stripe", url:"https://github.com/stripe/stripe-ios.git", from: "20.1.1")
+//        .package(name: "Stripe", url:"https://github.com/stripe/stripe-ios.git", from: "20.1.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "Stripe",
+            url: "https://github.com/stripe/stripe-ios/releases/download/v19.3.0/Stripe.xcframework.zip",
+            checksum: "fe459dd443beee5140018388fd6933e09b8787d5b473ec9c2234d75ff0d968bd"),
         .target(
             name: "CardScan",
-            dependencies: ["Stripe"]),
+            dependencies: ["Stripe", "UIKit"]),
+
+        //https://github.com/stripe/stripe-ios/releases/download/20.1.1/Stripe.xcframework.zip
+        //67865abafbe168f768bb40fe417f9977338f90847bfda25592fb33a22730d0c4
         .testTarget(
             name: "CardScanTests",
             dependencies: ["CardScan"])
