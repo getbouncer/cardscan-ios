8b7d4d24dc7d03bff2ea32e7ca95edc48f9e178b
jaimejiyepark <jaimejiyepark@gmail.com>
2020-12-02 16:52:42 -0800

    rename stripe in package manifest

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url:"https://github.com/stripe/stripe-ios.git", from: "20.1.1")
+        .package(name: "Stripe", url:"https://github.com/stripe/stripe-ios.git", from: "20.1.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
