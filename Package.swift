8ed84db1408498fe575ae7cf20a25acd606577cf
jaimejiyepark <42391849+jaimejiyepark@users.noreply.github.com>
2020-12-07 18:18:52 -0800

    Add Package.swift for binary SPM support (#242)

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "CardScan",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "CardScan",
+            targets: ["CardScan"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "CardScan",
+            url: "https://downloads.getbouncer.com/swift_package_manager/1.0.5052/CardScan.xcframework.zip",
+            checksum: "a521b121abe9edf712fb480a9ea1bddd5b994e4a205c2aae72ef1c6387a778db"
+        )
+    ]
+)
