caadb6c9b2059b3da7879e5c86be5f22a7295865
Sam King <kingst@gmail.com>
2021-01-15 12:55:08 -0800

    Adding podspec

---
+++ CardScan.xcframework/ios-arm64_x86_64-simulator/CardScan.framework/Headers/CardScan.h
@@ -0,0 +1,18 @@
+//
+//  CardScan.h
+//  CardScan
+//
+//  Created by Sam King on 12/13/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CardScan.
+FOUNDATION_EXPORT double CardScanVersionNumber;
+
+//! Project version string for CardScan.
+FOUNDATION_EXPORT const unsigned char CardScanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardScan/PublicHeader.h>
+
+
