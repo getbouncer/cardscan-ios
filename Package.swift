dc7768be56dbd7401bdae932003ef6412deded77
Sam King <kingst@gmail.com>
2021-02-11 12:39:14 -0800

    Commit for package.swift update on 2.0.8

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CardScan",
-            url: "https://downloads.getbouncer.com/swift_package_manager/production-20210211-1613075676/CardScan.xcframework.zip",
+            url: "https://downloads.getbouncer.com/swift_package_manager/2.0.8/CardScan.xcframework.zip",
             checksum: "12a3b5755f61bdf5ea819a68f467ec100b91c98d247cf3999efe24ef1a1905bb"
         )
     ]
