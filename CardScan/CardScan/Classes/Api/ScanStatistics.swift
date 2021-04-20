2642a7d4321b11abc081a30dad310035e067f157
jaimejiyepark <42391849+jaimejiyepark@users.noreply.github.com>
2021-04-20 14:14:00 -0700

    Make scan stats to encodable (#271)
    * scan stats to encodable
    
    * remove general device

---
+++ CardScan/CardScan/Classes/Api/ScanStatistics.swift
@@ -0,0 +1,88 @@
+//
+//  Stats.swift
+//  CardScan
+//
+//  Created by Jaime Park on 4/15/21.
+//
+
+import Foundation
+
+struct ScanStatisticsPayload: Encodable {
+    let device = ScanStatisticDevice()
+    let scanStats: ScanStatistics
+
+    enum CodingKeys: String, CodingKey {
+        case scanStats = "scan_stats"
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(scanStats, forKey: .scanStats)
+        try device.encode(to: encoder)
+    }
+}
+
+struct ScanStatisticDevice: Encodable {
+    let locale: String? = DeviceUtils.locale
+    let build: String = DeviceUtils.build
+    let osVersion: String = DeviceUtils.osVersion
+    let platform: String = DeviceUtils.platform
+    let sdkVersion = AppInfoUtils.getSdkVersion()
+    
+    enum CodingKeys: String, CodingKey {
+        case locale = "device_locale"
+        case build = "build"
+        case osVersion = "os"
+        case platform = "platform"
+        case sdkVersion = "sdk_version"
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(locale, forKey: .locale)
+        try container.encode(build, forKey: .build)
+        try container.encode(osVersion, forKey: .osVersion)
+        try container.encode(platform, forKey: .platform)
+        try container.encode(sdkVersion, forKey: .sdkVersion)
+    }
+}
+
+struct ScanStatistics: Encodable {
+    let scans: Int
+    let cardsDetected: Int
+    let torchOn: Bool
+    let orientation: String
+    let success: Bool
+    let duration: Double
+    let model: String
+    let permissionGranted: String
+    let deviceType: String = DeviceUtils.getDeviceType()
+    let userCanceled: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case scans = "scans"
+        case cardDetected = "cards_detected"
+        case torchOn = "torch_on"
+        case orientation = "orientation"
+        case success = "success"
+        case duration = "duration"
+        case model = "model"
+        case permissionGranted = "permission_granted"
+        case deviceType = "device_type"
+        case userCanceled = "user_canceled"
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(scans, forKey: .scans)
+        try container.encode(cardsDetected, forKey: .cardDetected)
+        try container.encode(torchOn, forKey: .torchOn)
+        try container.encode(orientation, forKey: .orientation)
+        try container.encode(success, forKey: .success)
+        try container.encode(duration, forKey: .duration)
+        try container.encode(model, forKey: .model)
+        try container.encode(permissionGranted, forKey: .permissionGranted)
+        try container.encode(deviceType, forKey: .deviceType)
+        try container.encode(userCanceled, forKey: .userCanceled)
+    }
+}
